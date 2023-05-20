f3dd5e0ead9860b6db0a99ebc4ec5cddffdb80bb
adietyachristian <adietya.christian@ui.ac.id>
2023-05-21 00:27:08 +0700

    fix: update database settings

--- core/settings.py
+++ core/settings.py
@@ -137,23 +137,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'toko-dizital',
-#         'USER': 'toko-dizital-instance',
-#         'PASSWORD': 'toko-dizital-2023',
-#         'HOST': '/cloudsql/toko-dizital:asia-southeast2:toko-dizital-instance',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toko-dizital',
+        'USER': 'toko-dizital-instance',
+        'PASSWORD': 'toko-dizital-2023',
+        'HOST': '/cloudsql/toko-dizital:asia-southeast2:toko-dizital-instance',
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {'default': env.db()}
 
