905449faad9dd0d7b60041f5372f29c0600ff4ff
adietyachristian <adietya.christian@ui.ac.id>
2023-05-25 15:21:53 +0700

    fix: fix pagination

--- history/serializers.py
+++ history/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from history.models import History
 from disease.serializers import DiseaseSerializer
 
+class PaginationSerializer(serializers.Serializer) :
+    total_pages = serializers.CharField()
+    current_page = serializers.CharField()
+    limit = serializers.CharField()
+    total_item = serializers.CharField()
+    has_next = serializers.BooleanField()
+    has_previous = serializers.BooleanField()
+
 class HistorySerializer(serializers.ModelSerializer):
     disease = DiseaseSerializer(read_only=True)
 
@@ -9,3 +17,7 @@ class HistorySerializer(serializers.ModelSerializer):
         model = History
         fields = '__all__'
         read_only_fields = ('timestamp', 'id')
+    
+class HistoryListSerializer(serializers.Serializer) :
+    result = HistorySerializer(many=True)
+    meta = PaginationSerializer()
--- history/views.py
+++ history/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import History
-from .serializers import HistorySerializer
+from .serializers import HistoryListSerializer
 from django.http import Http404
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated
@@ -13,14 +13,32 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 @method_decorator(csrf_exempt, name='dispatch')
 class HistoryAPI(APIView):
     def get(self, request):
         query_params = request.query_params
-        limit = int(query_params.get('limit', 10))
-        offset = int(query_params.get('offset', 0))
 
-        histories = History.objects.filter(user=request.user).order_by('-timestamp')[offset:offset+limit]
-        serializer = HistorySerializer(histories, many=True)
+        limit = int(query_params.get('limit', 10))
+        page = int(query_params.get('page', 1))
+
+        histories = History.objects.filter(user=request.user).order_by('-timestamp')
+        histories_paginator = Paginator(histories, limit)
+        history_page = histories_paginator.get_page(page)
+
+        data = {
+            "result" : history_page,
+            "meta" : {
+                "total_pages" : histories_paginator.num_pages,
+                "current_page" : page,
+                "limit" : limit,
+                "total_item" : histories_paginator.count,
+                "has_next" : history_page.has_next(),
+                "has_previous" : history_page.has_previous(),
+            }   
+        }
+
+        serializer = HistoryListSerializer(data)
+
         return Response(serializer.data)
