f4f2c1f36bf040f4cf80a3c8df9b54599f96d75b
adietyachristian <adietya.christian@ui.ac.id>
2023-06-05 01:05:10 +0700

    fix: blog serializer returns user data

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from blog.models import Blog
 from authentication.serializers import user_serializer
+from authentication.serializers.profile_serializer import ProfileSerializer
 
 class PaginationSerializer(serializers.Serializer) :
     total_pages = serializers.CharField()
@@ -11,6 +12,7 @@ class PaginationSerializer(serializers.Serializer) :
     has_previous = serializers.BooleanField()
 
 class BlogSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer()
 
     class Meta:
         model = Blog
