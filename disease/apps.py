d4cb27980c6445142327a3d28c42fa5f6b4ff260
aprilian tantra <tantraluhur@gmail.com>
2023-06-06 19:39:44 +0700

    change disease config

--- disease/apps.py
+++ disease/apps.py
@@ -8,26 +8,26 @@ class DiseaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'disease'
 
-    def ready(self) : 
-        FS = gcsfs.GCSFileSystem(project="toko-dizital",
-                         token="gcpCredentials.json")
-        with FS.open("gs://toko-dizital/model/model_type_3.h5", 'rb') as model_file:
-            model_gcs = h5py.File(model_file, 'r')
-            model = load_model(model_gcs)
+    # def ready(self) : 
+    #     FS = gcsfs.GCSFileSystem(project="toko-dizital",
+    #                      token="gcpCredentials.json")
+    #     with FS.open("gs://toko-dizital/model/model_type_3.h5", 'rb') as model_file:
+    #         model_gcs = h5py.File(model_file, 'r')
+    #         model = load_model(model_gcs)
 
-        self.model_type = model
+    #     self.model_type = model
 
-        print("Load Model Type Success!!!")
-        with FS.open("gs://toko-dizital/model/model_potato_4.h5", 'rb') as model_file:
-            model_gcs = h5py.File(model_file, 'r')
-            model = load_model(model_gcs)
+    #     print("Load Model Type Success!!!")
+    #     with FS.open("gs://toko-dizital/model/model_potato_4.h5", 'rb') as model_file:
+    #         model_gcs = h5py.File(model_file, 'r')
+    #         model = load_model(model_gcs)
 
-        self.potato_model = model
-        print("Load Model Potato Success!!!")
+    #     self.potato_model = model
+    #     print("Load Model Potato Success!!!")
 
-        with FS.open("gs://toko-dizital/model/model_apple_2.h5", 'rb') as model_file:
-            model_gcs = h5py.File(model_file, 'r')
-            model = load_model(model_gcs)
+    #     with FS.open("gs://toko-dizital/model/model_apple_2.h5", 'rb') as model_file:
+    #         model_gcs = h5py.File(model_file, 'r')
+    #         model = load_model(model_gcs)
 
-        self.apple_model = model
-        print("Load Model Apple Success!!!")
+    #     self.apple_model = model
+    #     print("Load Model Apple Success!!!")
